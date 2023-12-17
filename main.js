f4345015ea0c34e15d83932966d72ff958d0a71b
Ярцев О.Ю <132137309+Drukast@users.noreply.github.com>
2023-12-17 14:43:17 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+const BlogsElem = document.getElementById('list');
+let BlogList = []
+let DivList = []
+
+function CloseBlog(Div) {
+  for (let i = 0; i < DivList.length; i++) {
+    if (Div !== DivList[i]) {
+      DivList[i].lastChild.textContent = DivList[i].lastChild.textContent.split('\n')[0]
+      DivList[i].classList.remove('choosen');
+    }
+  }
+}
+
+function OpenBlog(Div) {
+  let DivID = Div.getAttribute('id')
+  BlogList.forEach(Blog => {
+    if (Blog['id'] == DivID) {
+      Div.lastChild.textContent = Blog['body']
+      Div.classList.add('choosen');
+    }
+  });
+}
+
+function AddBlogToList(blog) {
+  BlogList.push(blog)
+  let NewDiv = document.createElement('div');
+  NewDiv.onclick = function () {
+    CloseBlog(NewDiv)
+    OpenBlog(NewDiv)
+  }
+  NewDiv.setAttribute('id', blog['id'])
+  NewDiv.classList.add('blog');
+  let NewH3 = document.createElement('h3');
+  NewH3.classList.add('blogtitle');
+  let NewP = document.createElement('p');
+  NewP.classList.add('blogbody');
+
+  NewH3.textContent = blog['title'];
+  NewP.textContent = blog['body'].split('\n')[0];
+
+  NewDiv.append(NewH3);
+  NewDiv.append(NewP);
+  DivList.push(NewDiv);
+  BlogsElem.append(NewDiv);
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'GET',
+}).then((response) => response.json()).then((json) => json.forEach((blog) => AddBlogToList(blog)))
